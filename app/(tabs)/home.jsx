5625291e1bd711666ea4ded0b39375da6eee116d
Khushi <khushi.jirge@gmail.com>
2024-11-27 01:36:28 -0500

    more work

---
+++ app/(tabs)/home.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Header from '../../components/Home/Header'
+
+export default function Home() {
+  return (
+    <View style={{
+      padding:20, marginTop:5
+    }}>
+      <Header/>
+    </View>
+  )
+}
--- app/login/index.jsx
+++ app/login/index.jsx
@@ -1,9 +1,44 @@
 import { View, Text, Image, Pressable } from 'react-native'
-import React from 'react'
+import React, { useCallback, useEffect } from 'react'
 import Colors from './../../constants/Colors'
+import * as WebBrowser from 'expo-web-browser'
+import { useOAuth } from '@clerk/clerk-expo'
+import * as Linking from 'expo-linking'
+
+export const useWarmUpBrowser = () => {
+    useEffect(() => {
+      // Warm up the android browser to improve UX
+      // https://docs.expo.dev/guides/authentication/#improving-user-experience
+      void WebBrowser.warmUpAsync()
+      return () => {
+        void WebBrowser.coolDownAsync()
+      }
+    }, [])
+}
+  
+WebBrowser.maybeCompleteAuthSession()
 
 export default function LoginScreen() {
-  return (
+    useWarmUpBrowser()
+
+    const { startOAuthFlow } = useOAuth({ strategy: 'oauth_google' })
+
+    const onPress = useCallback(async () => {
+        try {
+          const { createdSessionId, signIn, signUp, setActive } = await startOAuthFlow({
+            redirectUrl: Linking.createURL('/(tabs)/home', { scheme: 'myapp' }),
+          })
+    
+          if (createdSessionId) {
+          } else {
+            // Use signIn or signUp for next steps such as MFA
+          }
+        } catch (err) {
+          console.error('OAuth error', err)
+        }
+    }, [])
+
+    return (
     <View style={{
         backgroundColor:Colors.WHITE,
         height: '100%'
@@ -31,7 +66,9 @@ export default function LoginScreen() {
             textAlign: 'center',
             color:Colors.GRAY
         }}>Let's adopt the pet which you like and make their life happy again</Text>
-        <Pressable style={{
+        <Pressable 
+        onPress={onPress}
+        style={{
             padding: 14,
             marginTop: 100,
             backgroundColor: Colors.PRIMARY,
